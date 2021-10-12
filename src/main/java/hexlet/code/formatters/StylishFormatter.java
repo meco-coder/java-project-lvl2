6579a29ebfe74fd6779599d0eae701e0dbf38f57
ivan_markov <zumkeee@gmail.com>
2021-10-12 20:07:02 +0400

    Hexlet

--- src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -1,29 +1,28 @@
 package hexlet.code.formatters;
 
 public class StylishFormatter {
-    private static String resultToString;
     private static StringBuilder result = new StringBuilder("{\n");
 
     public static void add(Object key, Object value) {
-        result.append(" + ").append(key).append(": ").append(value).append("\n");
+        result.append("  + ").append(key).append(": ").append(value).append("\n");
     }
 
     public static void unchanged(Object key, Object value) {
-        result.append("   ").append(key).append(": ").append(value).append("\n");
+        result.append("    ").append(key).append(": ").append(value).append("\n");
     }
 
     public static void change(Object key, Object value1, Object value2) {
-        result.append(" - ").append(key).append(": ").append(value1).append("\n")
-                .append(" + ").append(key).append(": ").append(value2).append("\n");
+        result.append("  - ").append(key).append(": ").append(value1).append("\n")
+                .append("  + ").append(key).append(": ").append(value2).append("\n");
     }
 
     public static void remove(Object key, Object value) {
-        result.append(" - ").append(key).append(": ").append(value).append("\n");
+        result.append("  - ").append(key).append(": ").append(value).append("\n");
     }
 
     public static String getResult() {
         result.append("}");
-        resultToString = result.toString();
+        String resultToString = result.toString();
         result = new StringBuilder("{\n");
         return resultToString;
     }
